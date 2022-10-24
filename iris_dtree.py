b27bc228a837dce576fb5735c7cab3643f7ad3ef
amirah264724 <115910597+amirah264724@users.noreply.github.com>
2022-10-24 13:51:56 +0800

    Update iris_dtree.py

--- iris_dtree.py
+++ iris_dtree.py
@@ -17,12 +17,10 @@ Xtrain, Xtest, ytrain, ytest = train_test_split(X_iris, y_iris,random_state=0)
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(Xtrain, ytrain)
 
-
-
 clf.fit(Xtrain, ytrain)
 
 tree.plot_tree(clf.fit(Xtrain, ytrain) )
 
 clf.score(Xtest, ytest)
 
-clf.score(Xtest, ytest)
+iris.info()

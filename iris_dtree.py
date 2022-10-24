a690f5bff541d033b89e239d6c6128f72e9d5b32
amirah264724 <115910597+amirah264724@users.noreply.github.com>
2022-10-24 14:26:39 +0800

    Update iris_dtree.py

--- iris_dtree.py
+++ iris_dtree.py
@@ -5,22 +5,22 @@ import matplotlib as plt
 import pandas as pd
 from sklearn.datasets import load_iris
 iris=sns.load_dataset('iris')
-X_iris = iris.drop('species', axis=1)  
+x_iris = iris.drop('species', axis=1)  
 y_iris = iris['species']
 
 from sklearn import tree
 from sklearn.tree import plot_tree
 from sklearn.model_selection import train_test_split
 
-Xtrain, Xtest, ytrain, ytest = train_test_split(X_iris, y_iris,random_state=1)
-clf.fit(Xtrain, ytrain)
+xtrain, xtest, ytrain, ytest = train_test_split(x_iris, y_iris,random_state=1)
+clf.fit(xtrain, ytrain)
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(Xtrain, ytrain)
+clf = clf.fit(xtrain, ytrain)
 
 fig=ply.figure(figsize=(10,4))
 
 
 
-tree.plot_tree(clf.fit(Xtrain, ytrain) )
+tree.plot_tree(clf.fit(xtrain, ytrain) )
 
-clf.score(Xtest, ytest)
+clf.score(xtest, ytest)

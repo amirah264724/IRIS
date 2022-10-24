4e988892982d752aa58251db682db3f0cf43eab1
amirah264724 <115910597+amirah264724@users.noreply.github.com>
2022-10-24 15:10:12 +0800

    Update iris_dtree.py

--- iris_dtree.py
+++ iris_dtree.py
@@ -1,24 +1,20 @@
 import streamlit as st
-import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt
-import pandas as pd
 from sklearn.datasets import load_iris
-iris=sns.load_dataset('iris')
-
-X_iris = iris.drop('species', axis=1)  
-y_iris = iris['species']
-
 from sklearn import tree
 from sklearn.tree import plot_tree
 from sklearn.model_selection import train_test_split
-
+iris=sns.load_dataset('iris')
+X_iris = iris.drop('species', axis=1)  
+y_iris = iris['species']
 Xtrain, Xtest, ytrain, ytest = train_test_split(X_iris, y_iris,random_state=1)
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(Xtrain, ytrain)
 
-fig = ply.figure(figsize=(10,4))
+
+
 clf.fit(Xtrain, ytrain)
+
 tree.plot_tree(clf.fit(Xtrain, ytrain) )
-st.pyplot(fig)
+
 clf.score(Xtest, ytest)
